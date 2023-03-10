b804ea2f8cb35d9680d32b6a3ac9e6e5f6365f65
Anuj Sharma <anujshandillya000@gmail.com>
2023-03-10 16:41:41 +0530

    pushed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ToDo from "./Components/ToDo";
+import Form from "./Components/Form";
+import FilterButton from "./Components/FilterButton";
+import { nanoid } from "nanoid";
 
-function App() {
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
+  function deleteTask(id) {
+    const remainingTask = tasks.filter((task) => id !== task.id );
+    setTasks(remainingTask);
+  }
+
+  function addTask(name){
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  
+
+  function toggleCompletedTask(id){
+    const updatedtask = tasks.map((task) => {
+      if(id === task.id){
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+
+    setTasks(updatedtask);
+  }
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <ToDo
+        id={task.id}
+        what={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleCompletedTask}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+  ));
+
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));   
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading">{taskList.length} {tasksNoun} remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
